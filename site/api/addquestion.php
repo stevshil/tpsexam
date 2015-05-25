32be0348ecc56c51d4028a5ff79148e9bb4c654c
Steve Shilling <steve@therapypages.com>
2015-05-25 20:59:02 +0100

    Working cli version of addquestion

--- site/api/addquestion.php
+++ site/api/addquestion.php
@@ -1,5 +1,25 @@
 <?php
-	function writeQuestionsAndAnswers() {
+	include 'dbfunc.php';
+	function writeQuestionsAndAnswers($question,$category,$answers) {
+		if ( isset($_SESSION['UserID']) ) {
+			$UserID=$_SESSION['UserID'];
+		} else {
+			$UserID=1;
+		}
+		if ( dbinsert('questions','Question,Added,AddedBy',"'$question',Now(),$UserID") ) {
+			# Add the category
+			$qid=dbquery('QID','questions'," WHERE Question='$question'");
+			if ( dbinsert('questionCategory','QID,CategoryID',$qid[0]['QID'] . ",$category") ) {
+				# If question inserts ok, insert the answers
+				for ( $counter=0; $counter <= (count($answers)-1); $counter+=2 ) {
+					$correct=$counter+1;
+					if ( ! dbinsert('answers','AID,QID,Correct,AnswerText,Added,AddedBy',"'" . $qid[0]['QID'] . "-$counter'," . $qid[0]['QID'] . ",'" . chop($answers[$correct]) . "','" . $answers[$counter] . "',Now(),$UserID") ) {
+						echo "DB insert failed to do\n";
+						echo "INSERT INTO answers (QID,Correct,AnswerText,Added,AddedBy) VALUES(" . $qid[0]['QID'] . ",'" . chop($answers[$correct]) . "','" . $answers[$counter] . "',Now(),$UserID)\n";
+					}
+				}
+			}
+		}
 	}
 
 	$websession=0;
@@ -24,26 +44,33 @@
 		$counter=1;
 		$numAnswers = "answer" . $counter;
 		$correctAnswers = "correct" . $counter;
-		while ( isset($_POST[$numAnswers] ) {
-			array_push($answers, array($_POST[$numAnswers] => "$correct"));
+		while ( isset($_POST[$numAnswers]) ) {
+			array_push($answers, $_POST[$numAnswers],"$correct");
 			$counter++;
 			$numAnswers = "answer" . $counter;
 			$correctAnswer = "correct" . $counter;
 		}
+		writeQuestionsAndAnswers($questions,$category,$answers);
 	} else {
 		# blulk load cmdline
-		$dataFile = fopen($argv[0],"r") or die("Unable ot open file!");
+		$dataFile = fopen($argv[1],"r") or die("Unable ot open file!");
 		while ( ! feof($dataFile) ) {
 			$dataRead = fgets($dataFile);
+			if ( ! preg_match('/\!/', $dataRead) ) {
+				$writeData=1;
+			}
 			# File format: Question!category!answer1!correct!answer2!correct!answer...!correct...
 			$fields=explode("!",$dataRead); # Breaks line into array
-			$questions=$fields[0];
-			$category=$fields[1];
-			for ( $counter = 2; $counter <= count($fields); $counter++ ) {
-				array_push($answers, array($fields[$counter],$fields[$counter+1]));
-				$counter+=2;
+			$questions=array_shift($fields);
+			$category=array_shift($fields);
+			for ( $counter = 0; $counter < (count($fields)-1) ; $counter+=2 ) {
+				array_push($answers, $fields[$counter],$fields[$counter+1]);
+			}
+			if ( ! isset($writeData) ) {
+				writeQuestionsAndAnswers($questions,$category,$answers);
 			}
 		}
+		$writeData=null;
 		fclose($dataFile);
 	}
 
